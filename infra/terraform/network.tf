26aafe26dae75269cc30a058762bd12dba753940
Ricardo César <ricardocesarfilho@unifor.br>
2025-01-15 17:22:50 -0300

    terraform-setup

--- infra/terraform/network.tf
+++ infra/terraform/network.tf
@@ -1,5 +1,30 @@
-resource "aws_vpc" "template"{
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support = true
+//resource "aws_vpc" "template"{
+//  cidr_block = "10.0.0.0/16"
+//  enable_dns_hostnames = true
+//  enable_dns_support = true
+//}
+
+locals {
+    azs = ["${var.aws_region}a"]
+    num_of_az = length(local.azs)
+    subnet_cidr = cidrsubnets(var.vpc_cidr, var.public_subnets_cidr_root_newbits)
+}
+
+locals {
+  public_subnets_list_cidr = cidrsubnets(local.subnet_cidr[0], [for i in range(local.num_of_az): var.public_subnets_cidr_sub_newbits]...)
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+  name = "${var.env_name}-vpc"
+  cidr = var.vpc_cidr
+
+  azs = local.azs
+  public_subnets  = local.public_subnets_list_cidr
+  
+  create_igw = true
+  map_public_ip_on_launch = true
 }
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,59 @@
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
+variable "env_name" {
+  type = string
+  default = "template"
+}
+variable "provisioner" {
+  type = string
+  default = "terraform"
+}
+variable "ec2_keypair_public_key" {
+  type = string
+}
+variable "ec2_keypair_private_key" {
+  type = string
+}
+variable "ec2_instance_type" {
+  type = string
+  default = "t2.micro"
+}
+variable "ami_id" {
+  type = string
+  default = "ami-0e9107ed11be76fde"
+}
+######################################################################
+## Network
+variable "vpc_cidr" {
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets_cidr_root_newbits" {
+  type = number
+  default = 4
+}
+
+variable "public_subnets_cidr_sub_newbits" {
+  type = number
+  default = 4
+}
+######################################################################
+variable "ingress_cidr_blocks"{
+  type = list
+  default = ["0.0.0.0/0"]
+}
+variable "ingress_rules" {
+  type = list
+  default = ["http-80-tcp", "ssh-tcp"]
+}
+variable "egress_rules" {
+  type = list
+  default = ["all-all"]
+}
