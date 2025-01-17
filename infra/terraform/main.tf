30897be5f37429d7db90efc023a49def96cc65df
Ricardo César <ricardocesarfilho@unifor.br>
2025-01-17 15:20:05 -0300

    a

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -13,9 +13,20 @@ resource "aws_key_pair" "ec2-key" {
   public_key = var.ec2_keypair_public_key
 }
 
-module "ec2_instance"{
+module "ec2_instance_1"{
   source = "terraform-aws-modules/ec2-instance/aws"
-  name = "${var.env_name}-instance"
+  name = "${var.env_name}-instance-1"
+  ami = var.ami_id
+  instance_type = var.ec2_instance_type
+
+  key_name = aws_key_pair.ec2-key.key_name
+  vpc_security_group_ids = [module.ec2_sg.security_group_id]
+  subnet_id = module.vpc.public_subnets[0]
+}
+
+module "ec2_instance_2"{
+  source = "terraform-aws-modules/ec2-instance/aws"
+  name = "${var.env_name}-instance-2"
   ami = var.ami_id
   instance_type = var.ec2_instance_type
 
--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,8 +1,12 @@
 resource "local_file" "inventory_file" {
   content = templatefile("./inventory.template",
     {
-      ec2_public_ip = [module.ec2_instance.public_ip]
+      ec2_public_ip = [module.ec2_instance_1.public_ip, module.ec2_instance_2.public_ip],
     }
   )
   filename = "./inventory"
 }
+#resource "local_file" "lb_public_ip" {
+#  content = module.vpc.nat_public_ips
+#  filename = "./public_ips"
+#}
