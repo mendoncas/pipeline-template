0f56d35a97c65adc6284ce7af405e960ee21975e
Ricardo César <ricardocesarfilho@unifor.br>
2025-01-19 15:42:36 -0300

    mensagem

--- src/main.go
+++ src/main.go
@@ -7,19 +7,19 @@ import (
 	"os"
 )
 
-func main(){
-  port, ok := os.LookupEnv("PORT")
-  if !ok {
-    port = "3000"
-  }
-  message, ok := os.LookupEnv("MESSAGE")
-  if !ok {
-    message = "This is the server´s default message! :3"
-  }
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-    io.WriteString(w, "Hello, DevOps! " + message)
-  })
-  log.Println("iniciando servidor na porta " + port)
-  err := http.ListenAndServe(":"+port , nil)
-  log.Println(err.Error())
+func main() {
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "3000"
+	}
+	message, ok := os.LookupEnv("MESSAGE")
+	if !ok {
+		message = "This is the server´s default message! :3"
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello, DevOps! This is a message from: "+message)
+	})
+	log.Println("iniciando servidor na porta " + port)
+	err := http.ListenAndServe(":"+port, nil)
+	log.Println(err.Error())
 }
